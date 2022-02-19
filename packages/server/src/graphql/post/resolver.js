cb0600a1fdb02daddfeee7fd240125766aedf8ba
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2022-02-19 12:57:39 +0800

    Kafka subscription

--- packages/server/src/graphql/post/resolver.js
+++ packages/server/src/graphql/post/resolver.js
@@ -1,6 +1,26 @@
-import { PubSub } from 'graphql-subscriptions';
+import * as Logger from 'bunyan';
 
-const pubsub = new PubSub();
+import { PubSub } from 'graphql-subscriptions';
+import { KafkaPubSub } from 'graphql-kafka-subscriptions';
+
+const localPubSub = new PubSub();
+
+const kafkaPubSub = new KafkaPubSub({
+  host: 'localhost',
+  port: '9092',
+  topic: 'post_events',
+  groupId: 'post_events_subscriber',
+  logger: Logger.createLogger({
+    name: 'pubsub',
+    stream: process.stdout,
+    level: 'debug',
+  }),
+  // globalConfig: {
+  //   debug: 'consumer',
+  // },
+});
+
+const pubsub = localPubSub || kafkaPubSub;
 
 const posts = [
   {
