85601dc279673c9f1903ca2efdd06d2990466d01
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2022-02-20 17:53:10 +0800

    Rest

---
+++ packages/rest/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 4002;
+
+app.get('/me', (req, res) => {
+  res.json({
+    id: 1,
+    username: '@ava',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`);
+});
